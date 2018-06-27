fbb753a57b26d9634aa24088015d162498c30e82
lastone9182 <lastone9182@gmail.com>
2018-06-27 11:27:40 +0900

    app run

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask_flatpages import FlatPages
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 pages = FlatPages(app)
-
-
 from .views import *
+
+
+if __name__ == '__main__':
+    app.run()
